f98114646f7e968c24d60efa7fcc45180c5e918b
FapAgent <105993931+FapAgent@users.noreply.github.com>
2022-12-27 16:17:27 +0200

    changes for the urls

--- js/success.js
+++ js/success.js
@@ -2,6 +2,6 @@ addEventListener("load", (event) => {
 
     localStorage.clear();  
     setTimeout(function() {
-       window.location.href = "http://localhost/";
+       window.location.href = "https://shop.kkbo.pro/";
     }, 3000);
    });
